780f6d29dc395b9e7244f39e964de06e3439c000
sumitsinha36 <sumitsinha36@gmail.com>
2017-08-18 23:50:54 +0530

    Initial commit for hinduism

---
+++ hindu/src/main/java/com/hindu/hindu/HinduApplication.java
@@ -0,0 +1,12 @@
+package com.hindu.hindu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HinduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HinduApplication.class, args);
+	}
+}
---
+++ hindu/src/test/java/com/hindu/hindu/HinduApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hindu.hindu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HinduApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
